12a1aebfe6d29ae78daf8c7a7b6639223c198dce
IThundxr <me@ithundxr.dev>
2024-10-30 20:55:00 -0400

    fix(consistency): Modify window pane item models to match the standard for vanilla and other mods

--- src/main/java/com/simibubi/create/foundation/data/WindowGen.java
+++ src/main/java/com/simibubi/create/foundation/data/WindowGen.java
@@ -222,9 +222,7 @@ public class WindowGen {
 			.loot((t, g) -> t.dropWhenSilkTouch(g))
 			.item()
 			.tag(Tags.Items.GLASS_PANES)
-			.model((c, p) -> p.withExistingParent(c.getName(), Create.asResource("item/pane"))
-				.texture("pane", sideTexture)
-				.texture("edge", topTexture))
+			.model((c, p) -> p.generated(c, sideTexture))
 			.build()
 			.register();
 	}
